f0d1c498ff00ae67421761c62d15f979eb6e707d
Caainã Jeronimo <caainaje@gmail.com>
2021-10-28 22:14:21 -0300

    add rounded to skeletons

--- src/components/MovieDetailSkeleton.tsx
+++ src/components/MovieDetailSkeleton.tsx
@@ -3,8 +3,8 @@ export const MovieDetailSkeleton = () => {
     <>
       <div className="container mx-auto md:p-8 animate-pulse">
         <div className="flex flex-col space-y-2 pt-2 md:flex-row md:space-x-2 md:space-y-0">
-          <div className="w-64 md:w-80 mx-auto h-96 bg-gray-300" />
-          <div className="w-full bg-gray-300 shadow overflow-hidden sm:rounded-lg"></div>
+          <div className="w-64 md:w-80 mx-auto h-96 bg-gray-300 rounded-lg" />
+          <div className="w-full bg-gray-300 shadow overflow-hidden rounded-lg"></div>
         </div>
       </div>
     </>
--- src/components/MovieItemSkeleton.tsx
+++ src/components/MovieItemSkeleton.tsx
@@ -1,7 +1,7 @@
 export const MovieItemSkeleton = () => {
   return (
     <div className="animate-pulse">
-      <div className="bg-gray-300 w-40 flex items-center justify-center p-2 cursor-pointer h-60"></div>
+      <div className="bg-gray-300 w-40 flex items-center justify-center p-2 cursor-pointer h-60 rounded-lg"></div>
     </div>
   );
 };
